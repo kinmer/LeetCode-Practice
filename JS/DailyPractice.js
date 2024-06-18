0f566ed2f542062a1823e6b5d59d0ffab1d67ef0
yongZhang <kinmer7@gmail.com>
2024-06-17 23:09:42 -0400

    day 1

---
+++ JS/DailyPractice.js
@@ -0,0 +1,10 @@
+// Write a function createHelloWorld. It should return a new function that always returns "Hello World".
+
+function createHelloWorld(){
+    return function(){
+     return "Hello World";
+    };
+ }
+ 
+ const a = createHelloWorld();
+ console.log(a());
