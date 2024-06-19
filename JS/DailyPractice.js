3db01b544544974ce07cd61664eaa5b81ee22277
yongZhang <kinmer7@gmail.com>
2024-06-19 10:04:58 -0400

    counter

--- JS/DailyPractice.js
+++ JS/DailyPractice.js
@@ -32,3 +32,18 @@ const endInY = spices.filter(s => s.nickname.slice(-1) === "y")
 
 console.log(endInY);
 
+// Given an integer n, return a counter function. This counter function initially returns n and then returns 1 more than the previous value every subsequent time it is called (n, n + 1, n + 2, etc).
+
+
+const createCounter = (n) => {
+    let current = n;
+    const counter = () => {
+       return current ++;
+    }
+    return counter;
+}
+
+const counter = createCounter(10);
+console.log(counter());
+console.log(counter()); 
+console.log(counter()); 
