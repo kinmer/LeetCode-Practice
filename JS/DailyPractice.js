64282141446bb33de9790c5a560ffc852ecc9f65
yongZhang <kinmer7@gmail.com>
2024-06-20 14:47:29 -0400

    to be or not to be

--- JS/DailyPractice.js
+++ JS/DailyPractice.js
@@ -47,3 +47,25 @@ const counter = createCounter(10);
 console.log(counter());
 console.log(counter()); 
 console.log(counter()); 
+
+// Write a function expect that helps developers test their code. It should take in any value val and return an object with the following two functions.
+
+// toBe(val) accepts another value and returns true if the two values === each other. If they are not equal, it should throw an error "Not Equal".
+// notToBe(val) accepts another value and returns true if the two values !== each other. If they are equal, it should throw an error "Equal".
+const expect = (x) => {
+    return {
+        toBe : function(y) {
+            if (x === y) {
+                return true;
+            } else return false;
+        },
+        notToBe : function(z) {
+            if (x !== z) {
+                return true;
+            } else return 'Equal';
+        }
+    }
+}
+
+console.log(expect(5).toBe(5));
+console.log(expect(5).notToBe(5));
