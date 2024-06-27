585fc18b5cd1fe9e4e1f3e76573d178202935c25
yongZhang <kinmer7@gmail.com>
2024-06-27 16:14:43 -0400

    without map

--- JS/DailyPractice.js
+++ JS/DailyPractice.js
@@ -98,3 +98,17 @@ const counter = createCounter(5);
 console.log(counter.increment()); 
 console.log(counter.reset()); 
 console.log(counter.decrement()); 
+
+// Apply Transform Over Each Element in Array
+
+// Given an integer array arr and a mapping function fn, return a new array with a transformation applied to each element.
+// The returned array should be created such that returnedArray[i] = fn(arr[i], i).
+// Please solve it without the built-in Array.map method.
+const map = (arr, fn) => {
+    const newArr = [];
+    for (n of arr) {
+        i = arr.indexOf(n);
+       newArr.push(fn(n, i))
+    }
+    return newArr;
+}
