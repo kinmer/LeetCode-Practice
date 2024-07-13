4bb3d5ca02b708bd8076c8076610500763cd2fd1
yongZhang <kinmer7@gmail.com>
2024-07-12 22:16:51 -0400

    array last

--- JS/DailyPractice.js
+++ JS/DailyPractice.js
@@ -204,3 +204,10 @@ var chunk = function(arr, size) {
     }
     return chunkArray;
 };
+Array.prototype.last = function() {
+    if (this.length) {
+        return this[this.length -1]
+    } else {
+        return -1
+    }
+};
