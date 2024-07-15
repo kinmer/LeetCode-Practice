9f423146be3011df7f18e20fd37d1759ae1d10f5
yongZhang <kinmer7@gmail.com>
2024-07-15 15:19:22 -0400

    grouped

--- JS/DailyPractice.js
+++ JS/DailyPractice.js
@@ -211,3 +211,22 @@ Array.prototype.last = function() {
         return -1
     }
 };
+
+Array.prototype.groupBy = function(fn) {
+    // Reduce the array into a single object
+    return this.reduce((grouped, item) => {
+      // Apply the provided callback function to get the key
+      const key = fn(item);
+      
+      // If the key doesn't exist in the grouped object, create a new array fßsßor it
+      if (!grouped[key]) {
+        grouped[key] = [];
+      }
+      
+      // Push the current item to the array associated with the key
+      grouped[key].push(item);
+      
+      // Return the updated grouped object for the next iteration
+      return grouped;
+    }, {});
+  };ß
