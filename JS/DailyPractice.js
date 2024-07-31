62e2058207978cedf9b15c9b4169c2c55c842572
yongZhang <kinmer7@gmail.com>
2024-07-30 23:13:18 -0400

    compact object

--- JS/DailyPractice.js
+++ JS/DailyPractice.js
@@ -364,3 +364,17 @@ var flat = function(arr, depth) {
     return result.reverse();
   };
   
+  var compactObject = function(obj) {
+    if (obj === null) return null;
+    if (Array.isArray(obj)) return obj.filter(Boolean).map(compactObject);
+    if (typeof obj !== "object") return obj;
+    
+    const compacted = {};
+    
+    for (const key in obj) {
+        let value = compactObject(obj[key]);
+        if (Boolean(value)) compacted[key] = value;
+    }
+
+    return compacted;
+};
