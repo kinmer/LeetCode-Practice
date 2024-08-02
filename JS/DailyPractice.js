7cac1e0623dc6d72e5cde5e545731e9cc0a250d0
yongZhang <kinmer7@gmail.com>
2024-08-01 23:41:22 -0400

    Array Wrapper

--- JS/DailyPractice.js
+++ JS/DailyPractice.js
@@ -420,3 +420,31 @@ class EventEmitter {
       return results;
     }
   }
+
+  var ArrayWrapper = function (nums) {
+    this.nums = nums
+};
+
+/**
+ * @return {number}
+ */
+ArrayWrapper.prototype.valueOf = function () {
+
+    if (this.nums.length == 0) {
+        return 0
+    } else {
+        let sum = 0;
+        for (let i of this.nums) {
+            sum += i
+        }
+        return sum
+    }
+}
+
+/**
+ * @return {string}
+ */
+ArrayWrapper.prototype.toString = function () {
+    return JSON.stringify(this.nums)
+
+}
